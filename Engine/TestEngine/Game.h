73329ebdb441251885180106d58cedb5963d38c9
Alejandro <ale.bros@hotmail.com>
2019-04-01 19:02:15 -0300

    Engine 2D Commit

---
+++ Engine/TestEngine/Game.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "GameBase.h"
+#include "Triangle.h"
+#include "Rectangle.h"
+#include "Circle.h"
+#include "Sprite.h"
+#include "Tilemap.h"
+#include "Input.h"
+#include "CollisionManager.h"
+#include "Player.h"
+#include "Asteroid.h"
+
+enum GameState
+{
+	LOSE = -1,
+	CONTINUE,
+	WIN
+};
+
+class Game : public GameBase
+{
+	Input* input;
+	Material* material;
+	Material* matTexture;
+	Player* player;
+	list<Asteroid*>* asteroids;
+	Tilemap* tilemap;
+
+	float speed;		// Speed
+
+	const int totalAsteroids = 25;
+
+	GameState gameState;
+
+	void FillAsteroidsData();
+	void Restart();
+
+protected:
+	bool OnStart() override;
+	bool OnStop() override;
+	bool OnUpdate() override;
+	bool OnDraw() override;
+
+public:
+	Game();
+	~Game();
+};
+
